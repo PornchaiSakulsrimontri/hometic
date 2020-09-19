6a4dbec3f8a5f5bb25c1e6acf750573e8d8bf4cf
Pornchai Sakulsrimontri <pornchai.sak@kbtg.tech>
2020-09-19 15:02:47 +0700

    read data from request body

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,13 +10,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Pair struct {
+	DeviceID int64
+	UserID   int64
+}
+
 func main() {
 	fmt.Println("Hello hometic : I'm Gopher!!")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"status":"active"}`))
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
@@ -26,4 +30,21 @@ func main() {
 
 	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
+}
+
+func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	//var p Pair
+	var p Pair
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+	defer r.Body.Close()
+	fmt.Println("%v", p)
+	//read r.Body
+	//Println
+
+	w.Write([]byte(`{"status":"active"}`))
 }
